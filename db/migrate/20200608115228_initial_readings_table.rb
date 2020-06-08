8504e4d1a860aa17fc4bab2bf06daf925f5cca0f
Jacob Chae <jbcden@gmail.com>
2020-06-08 21:15:59 +0900

    Initial schema with tables
    This schema has a lot of potential issues -- noted in `import_notes.md`
    -- but this is just so that we can get an initial spike of the
    application running.

---
+++ db/migrate/20200608115228_initial_readings_table.rb
@@ -0,0 +1,17 @@
+class InitialReadingsTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :readings do |t|
+      t.string :reading
+      t.boolean :primary
+      # not sure this is useful but it will at least help us keep track of the
+      # last time we generated the db
+      t.timestamps
+    end
+
+    create_table :definitions do |t|
+      t.string :string
+      t.references :reading
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_08_115228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "definitions", force: :cascade do |t|
+    t.string "string"
+    t.bigint "reading_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["reading_id"], name: "index_definitions_on_reading_id"
+  end
+
+  create_table "readings", force: :cascade do |t|
+    t.string "reading"
+    t.boolean "primary"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 end
