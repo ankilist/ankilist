27cb36ad9bbc150f708cf4f5069b5661f6422db9
Jacob Chae <jbcden@gmail.com>
2020-06-10 22:48:05 +0900

    Initial import script
    Still needs testing

--- bin/import_dictionary.rb
+++ bin/import_dictionary.rb
@@ -10,14 +10,53 @@
 #  counter += 1
 #end
 
+count = 0
 doc = Nokogiri::XML(File.open("#{Rails.root}/JMdict_e"))
 
-entry = doc.search('//entry')[8]
+doc.search('//entry').each do |entry|
+  primary_reading = nil
+  readings = []
+  # p entry.elements
+  # Weird case on line 534
+  kebs = entry.elements.search('./keb')
+  if kebs.present?
+    # pp kebs
+    primary_reading = kebs.first
+    readings << Reading.create!(reading: primary_reading.content, primary: true)
 
-p entry.elements
-# Weird case on line 534
-pp entry.elements.search('./keb')
-pp entry.elements.search('./reb')
-pp entry.elements.search('../sense')[3].search('./gloss').map(&:content).join('; ')
+    kebs[1..].each do |non_primary_reading|
+      readings << Reading.create!(reading: non_primary_reading.content, primary: false)
+    end
+  end
+
+  # pp entry.elements.search('./reb')
+  rebs = entry.elements.search('./reb')
+  if primary_reading.nil? && rebs.present?
+    reb = rebs.first
+    readings << Reading.create!(reading: reb.content, primary: true)
+
+    rebs[1..].each do |non_primary_reading|
+      readings << Reading.create!(reading: non_primary_reading.content, primary: false)
+    end
+  else
+    rebs.each do |non_primary_reading|
+      readings << Reading.create!(reading: non_primary_reading.content, primary: false)
+    end
+  end
+
+  # pp entry.elements.search('../sense')[3].search('./gloss').map(&:content).join('; ')
+  entry.elements.search('../sense').each do |sense|
+    definition_entry = sense.search('./gloss').map(&:content).join('; ')
+
+    readings.each do |reading|
+      Definition.create!(reading_id: reading.id, definition: definition_entry)
+    end
+  end
+
+  count += 1
+  puts "MILLIONS" if count % 1_000_000 == 0
+  # exit if count == 100
+end
+
 # pp entry.method(:search).source_location
 # pp entry.public_methods - Object.public_methods
