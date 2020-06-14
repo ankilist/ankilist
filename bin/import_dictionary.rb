faf2f4e6e4d473d1c5776ebadf4e343163546c5f
Jacob Chae <jbcden@gmail.com>
2020-06-14 20:27:03 +0900

    Update import script to handle parts of speech

--- bin/import_dictionary.rb
+++ bin/import_dictionary.rb
@@ -1,62 +1,47 @@
 # frozen_string_literal: true
 
-#reader = Nokogiri::XML::Reader(File.open("#{Rails.root}/JMdict_e"))
-#counter = 0
-#
-#reader.each do |node|
-#  puts node.name
-#  puts "INNER: #{node.inner_xml}" if node.name == 'reb'
-#  exit if counter > 20
-#  counter += 1
-#end
-
 count = 0
 doc = Nokogiri::XML(File.open("#{Rails.root}/JMdict_e"))
 
 doc.search('//entry').each do |entry|
+  word = Word.create!
+
   primary_reading = nil
+  definition_part_of_speech = nil
+  definition_misc_info = nil
   readings = []
-  # p entry.elements
+
   # Weird case on line 534
   kebs = entry.elements.search('./keb')
   if kebs.present?
-    # pp kebs
     primary_reading = kebs.first
-    readings << Reading.create!(reading: primary_reading.content, primary: true)
+    readings << Reading.create!(reading: primary_reading.content, primary: true, word_id: word.id)
 
     kebs[1..].each do |non_primary_reading|
-      readings << Reading.create!(reading: non_primary_reading.content, primary: false)
+      readings << Reading.create!(reading: non_primary_reading.content, primary: false, word_id: word.id)
     end
   end
 
-  # pp entry.elements.search('./reb')
   rebs = entry.elements.search('./reb')
-  if primary_reading.nil? && rebs.present?
-    reb = rebs.first
-    readings << Reading.create!(reading: reb.content, primary: true)
+  is_primary_reading = primary_reading.nil? && rebs.present?
+  reb = rebs.first
+  readings << Reading.create!(reading: reb.content, primary: is_primary_reading, primary_kana: true, word_id: word.id)
 
-    rebs[1..].each do |non_primary_reading|
-      readings << Reading.create!(reading: non_primary_reading.content, primary: false)
-    end
-  else
-    rebs.each do |non_primary_reading|
-      readings << Reading.create!(reading: non_primary_reading.content, primary: false)
-    end
+  rebs[1..].each do |non_primary_reading|
+    readings << Reading.create!(reading: non_primary_reading.content, primary: false, primary_kana: false, word_id: word.id)
   end
 
-  # pp entry.elements.search('../sense')[3].search('./gloss').map(&:content).join('; ')
   entry.elements.search('../sense').each do |sense|
+    definition_part_of_speech = sense.search('./pos').map(&:content).join(', ') if sense.search('./pos').present?
+    definition_misc_info = sense.search('./misc').map(&:content).join(', ') if sense.search('./misc').present?
     definition_entry = sense.search('./gloss').map(&:content).join('; ')
 
-    readings.each do |reading|
-      Definition.create!(reading_id: reading.id, definition: definition_entry)
-    end
+    Definition.create!(definition: definition_entry,
+                       part_of_speech: definition_part_of_speech,
+                       misc_info: definition_misc_info, word_id: word.id)
   end
 
   count += 1
-  puts "MILLIONS" if count % 1_000_000 == 0
+  # puts 'PROGRESS!!!' if (count % 50_000).zero?
   # exit if count == 100
 end
-
-# pp entry.method(:search).source_location
-# pp entry.public_methods - Object.public_methods
