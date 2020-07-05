079d0f4fa80e242d541de55ab05bcc6c4a27995a
Jacob Chae <jbcden@gmail.com>
2020-07-05 14:40:44 +0900

    Initial typescript configuration

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,17 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
+const path = require("path")
+
+environment.plugins.append(
+  "ForkTsCheckerWebpackPlugin",
+  new ForkTsCheckerWebpackPlugin({
+    typescript: {
+      tsconfig: path.resolve(__dirname, "../../tsconfig.json")
+    },
+    async: false
+  })
+)
 
 module.exports = environment.toWebpackConfig()
