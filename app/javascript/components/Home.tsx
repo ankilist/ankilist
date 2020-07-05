642e86c79e969fd1254ab2e034f6458621de6e9e
Jacob Chae <jbcden@gmail.com>
2020-07-05 16:24:49 +0900

    Add initial card styling

--- app/javascript/components/Home.tsx
+++ app/javascript/components/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Card from './Card';
 
 export default () => (
 <div className="grid">
   <h1>AnkiList</h1>
   <h2>Turn Language lists into Anki cards effortlessly</h2>
+  <section className="card-section">
+    <Card />
+  </section>
 </div>
 );
