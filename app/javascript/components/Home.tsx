6a67f9b3c471c20d61c8f334359b15204c6e79af
Jacob Chae <jbcden@gmail.com>
2020-08-10 16:15:50 +0900

    Fix indentation

--- app/javascript/components/Home.tsx
+++ app/javascript/components/Home.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Card from './Card';
 
 export default () => (
-<div className="grid">
-  <h1>AnkiList</h1>
-  <h2>Turn Language lists into Anki cards effortlessly</h2>
-  <section className="card-section">
-    <Card header="Upload a list" body="Upload a text file containing words to be converted" button="Upload"/>
-    <Card header="Paste a list" body="Paste a list of words directly to be converted" button="Paste"/>
-  </section>
-</div>
+  <div className="grid">
+    <h1>AnkiList</h1>
+    <h2>Turn Language lists into Anki cards effortlessly</h2>
+    <section className="card-section">
+      <Card header="Upload a list" body="Upload a text file containing words to be converted" button="Upload"/>
+      <Card header="Paste a list" body="Paste a list of words directly to be converted" button="Paste"/>
+    </section>
+  </div>
 );
